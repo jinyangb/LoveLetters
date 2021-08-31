01597d9639e9bbc035581d9e8d926a0177756b23
jinyangb <your_email@example.com>
2021-08-30 23:16:18 -0400

    post.jsx added

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 function Nav() {
   return (
     <nav className="navbar">
-      <h4>Video Game To Do List</h4>
+      <h4>Love Letters</h4>
       <div>
         <NavLink to="/"></NavLink>
         <NavLink to="/about">About</NavLink>
---
+++ client/src/pages/Posts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PostList from '../components/PostList'
+
+function Home(props) {
+  return (
+    <div>
+      <PostList />
+    </div>
+  )
+}
+export default Home
