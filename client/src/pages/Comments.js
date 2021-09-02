13e6b7e3351a3636bb6aeb75a383c5fa7e6145e5
jinyangb <your_email@example.com>
2021-09-01 23:31:26 -0400

    fixed letters

--- client/src/pages/Comments.js
+++ client/src/pages/Comments.js
@@ -10,56 +10,55 @@ const Comments = () => {
 
   const handleChange = (e) => {
     console.log('test')
-    setComment({ ...newComment, [e.target.name]: e.target.value })
-
-    const addNewComment = async (formdata) => {
-      try {
-        const res = await axios.post(
-          process.env.NODE_ENV === 'production'
-            ? `${window.location.origin}/api/comments`
-            : 'http://localhost:3001/api/comments',
-          formdata
-        )
-
-        return res.data
-      } catch (error) {
-        console.log(error)
-      }
-    }
-
-    const getComments = async () => {
-      const res = await axios.get('http://localhost:3001/api/comments')
-      setComments(res.data.comments)
-    }
-
-    useEffect(() => {
-      getComments()
-    }, [])
-
-    const handleSubmit = (e) => {
-      e.preventDefault()
-      console.log('handlesubmit')
-      addNewComment(comment)
-    }
-    return (
-      <div className="list">
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            onChange={(e) => {
-              handleChange(e)
-            }}
-            name={'comment'}
-            placeholder={'comment'}
-          />
-          <button>Submit</button>
-        </form>
-        {comments.map((comment) => {
-          ;<div>{comment.comment}</div>
-        })}
-      </div>
-    )
+    setComment({ ...comment, [e.target.name]: e.target.value })
   }
+  const addNewComment = async (formdata) => {
+    try {
+      const res = await axios.post(
+        process.env.NODE_ENV === 'production'
+          ? `${window.location.origin}/api/comments`
+          : 'http://localhost:3001/api/comments',
+        formdata
+      )
+
+      return res.data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getComments = async () => {
+    const res = await axios.get('http://localhost:3001/api/comments')
+    setComments(res.data.comments)
+  }
+
+  useEffect(() => {
+    getComments()
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('handlesubmit')
+    addNewComment(comment)
+  }
+  return (
+    <div className="list">
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => {
+            handleChange(e)
+          }}
+          name={'comment'}
+          placeholder={'comment'}
+        />
+        <button>Submit</button>
+      </form>
+      {comments.map((comment) => {
+        ;<div>{comment.comment}</div>
+      })}
+    </div>
+  )
 }
 
 export default Comments
