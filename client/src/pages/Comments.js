f746aa4932795ba99a7075be42f05914208aebf2
jinyangb <your_email@example.com>
2021-09-01 22:06:42 -0400

    added delete

---
+++ client/src/pages/Comments.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react'
+// import TextInput from './TextInput'
+import axios from 'axios'
+
+const Comments = () => {
+  const [comment, setComment] = useState({
+    comment: ''
+  })
+  const [comments, setComments] = useState([])
+
+  const handleChange = (e) => {
+    console.log('test')
+    setComment({ ...newComment, [e.target.name]: e.target.value })
+
+    const addNewComment = async (formdata) => {
+      try {
+        const res = await axios.post(
+          process.env.NODE_ENV === 'production'
+            ? `${window.location.origin}/api/comments`
+            : 'http://localhost:3001/api/comments',
+          formdata
+        )
+
+        return res.data
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    const getComments = async () => {
+      const res = await axios.get('http://localhost:3001/api/comments')
+      setComments(res.data.comments)
+    }
+
+    useEffect(() => {
+      getComments()
+    }, [])
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      console.log('handlesubmit')
+      addNewComment(comment)
+    }
+    return (
+      <div className="list">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            onChange={(e) => {
+              handleChange(e)
+            }}
+            name={'comment'}
+            placeholder={'comment'}
+          />
+          <button>Submit</button>
+        </form>
+        {comments.map((comment) => {
+          ;<div>{comment.comment}</div>
+        })}
+      </div>
+    )
+  }
+}
+
+export default Comments
